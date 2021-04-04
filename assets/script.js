63f1514624c7e9ebb754ecb0b47190821f6baf5c
Ollie Bennett <olliebennett@gmail.com>
2021-04-04 08:55:22 +0100

    StandardJS fixes

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,31 @@
-
-//console.log("Letters:");
-//console.log(letters);
+/* globals $, symbols */
 
 // Disable form submission (eg. via Enter key)
-$('form').submit(false);
+$('form').submit(false)
 
-$("textarea, input, select").on("change keyup", function() {
-update_qrcode();
-});
+$('textarea, input, select').on('change keyup', function () {
+  updateQrCode()
+})
 
-var update_qrcode = function() {
+const updateQrCode = function () {
+  const size = 350
 
-  var size = 350;
-
-  $("#error-container").hide();
-  $("#qr").empty();
-  $("#qr").qrcode({
-    render: $("input[name=qr-format]:checked").val(),
+  $('#error-container').hide()
+  $('#qr').empty()
+  $('#qr').qrcode({
+    render: $('input[name=qr-format]:checked').val(),
     width: size,
     height: size,
     size: size,
     color: '#3a3',
-    text: $("#qr-data").val(),
-    textOverlay: $("#qr-overlay").val(),
-    textOverlayTransparency: !!! $("#qr-overlay-transparency").is(':checked'),
-    textOverlayBorder: $("#qr-overlay-border").is(':checked'),
+    text: $('#qr-data').val(),
+    textOverlay: $('#qr-overlay').val(),
+    textOverlayTransparency: !$('#qr-overlay-transparency').is(':checked'),
+    textOverlayBorder: $('#qr-overlay-border').is(':checked'),
     ecLevel: 'H',
     minVersion: 5,
-    symbols: symbols[$("#qr-overlay-font").val()]
-  });
-
+    symbols: symbols[$('#qr-overlay-font').val()]
+  })
 }
 
-update_qrcode();
+updateQrCode()
